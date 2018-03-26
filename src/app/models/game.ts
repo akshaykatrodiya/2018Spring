9e3a80505429a4f3fde40f737255dfcecb56ca9d
Akshay Katrodiya <katro@DESKTOP-2V0D8QR>
2018-03-26 10:00:43 -0400

    ngFor in html

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -7,9 +7,14 @@ export class Game {
         "Axis of Evil",
         "There are two extremes"
     ];
-    players: User[] = [];
-    picture: string = '';
-    playedQuotes: Quote[] = [];
+    playedQuotes: Quote[] = [
+        { text: "Let's do it!", playerName:'Akshay', chosen: true }
+    ];
+    players: User[] = [
+        { name: 'Akshay' }
+    ];
+    picture: string = 'https://media3.s-nbcnews.com/j/msnbc/components/video/201803/tdy_news_welker_trump_180315_1920x1080.nbcnews-ux-1080-600.jpg';
+    
 }
 
 export class User {
